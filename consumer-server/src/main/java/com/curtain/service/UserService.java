f6ebcf1c970a0021c131844a93ebaf1ce0479654
Curtain-Wang <wangqt1997@qq.com>
2021-03-17 22:36:02 +0800

    test所需要的 complier 组件

--- consumer-server/src/main/java/com/curtain/service/UserService.java
+++ consumer-server/src/main/java/com/curtain/service/UserService.java
@@ -1,8 +1,5 @@
 package com.curtain.service;
 
-import org.apache.dubbo.config.annotation.Reference;
-import org.springframework.stereotype.Service;
-
 public interface UserService {
     
     void buyTicket();
